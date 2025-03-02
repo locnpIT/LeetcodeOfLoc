62d9dc45f64efdf1d44a10d5612e77d5487ed52c
locnpIT <npl2004qn@gmail.com>
2025-03-02 23:31:43 +0700

    upload 1431 leetcode

---
+++ _1431_KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _1431_KidsWithTheGreatestNumberOfCandies {
+    public static void main(String[] args) {
+        int[] candies = {12, 1, 12};
+        int extraCandies = 10;
+        List<Boolean> result = kidsWithCandies(candies, extraCandies);
+        System.out.println(result);
+
+        
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = maxInArray(candies);
+        int len = candies.length;
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            if (candies[i] + extraCandies >= max) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+
+    static int maxInArray(int[] arr) {
+        int len = arr.length;
+        int max = 0;
+        for (int i = 0; i < len; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+}
