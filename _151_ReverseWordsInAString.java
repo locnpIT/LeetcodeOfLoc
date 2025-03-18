593efaff9a7d2608b947a2222b24c92b2bf84add
locnpIT <npl2004qn@gmail.com>
2025-03-18 23:46:55 +0700

    upload code

---
+++ _151_ReverseWordsInAString.java
@@ -0,0 +1,23 @@
+public class _151_ReverseWordsInAString {
+    
+    public static void main(String[] args) {
+
+        System.out.println(reverseWords("Nguyen Phuoc     Loc   "));
+        
+    }
+    
+    public static String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+            
+        s = s.trim();
+        String[] arr = s.split("\\s+");
+        int len = arr.length;
+        for(int i = len - 1; i >= 0; i--){
+            if(i != 0)
+                sb.append(arr[i] + " ");
+            else
+                sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+}
