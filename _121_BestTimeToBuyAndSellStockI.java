c662e2c91f783cb87849eae5cd3934012d972047
locnpIT <npl2004qn@gmail.com>
2025-03-20 23:54:21 +0700

    upload

---
+++ _121_BestTimeToBuyAndSellStockI.java
@@ -0,0 +1,15 @@
+public class _121_BestTimeToBuyAndSellStockI {
+    public static void main(String[] args) {
+        
+    }
+
+   
+        public int maxProfit(int[] prices) {
+            int mini=prices[0],profit=0;
+            for(int i=1;i<prices.length;i++){
+                profit=Math.max(profit,(prices[i]-mini));
+                mini=Math.min(mini,prices[i]);
+            }
+            return profit;
+        }
+}
