3abbb895943e3ea51ea9be6c0747d6b647802645
locnpIT <npl2004qn@gmail.com>
2025-05-12 22:56:36 +0700

    upload phan tich 1 so thanh thua so nguyen to

---
+++ PhanTichThuaSoNguyenTo.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhanTichThuaSoNguyenTo {
+    
+
+    public static List<Integer> phanTich(int n){
+        List<Integer> results = new ArrayList<>();
+
+        int num = 2;
+        while(n > 1){
+            if(n % num == 0){
+                results.add(num);
+                n /= num;
+            }
+            else{
+                num +=1;
+            }
+        }
+
+        return results;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(phanTich(12334));
+        
+    }
+}
